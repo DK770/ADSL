e56d30475b5a80f247a4ac48506d3af5bd9b098c
dhruv <dkanekal@gmail.com>
2020-02-17 15:42:26 +0530

    prac6 half done

--- prac6.cpp
+++ prac6.cpp
@@ -18,15 +18,88 @@ using namespace std;
 class word
 {
 	string key,value;
+public:
+	word()
+	{
+		key="\0";
+		value="\0";
+	}
 	friend class dictionary;
 };
 
 class dictionary
 {
-
+	word w[26][2];
+public:
+	void input();
+	void output();
+	int hashfunc(char);
 };
 
+void dictionary::input()
+{
+	int a,addr=0;
+	cout<<"Number of words in dictionary?:\t";
+	cin>>a;
+	for(int i=0;i<a;i++)
+	{
+		string s,s1;
+		cout<<"Enter keyword no. "<<i+1<<":-\t";
+		cin>>s;
+		addr=hashfunc(s[0]);
+		if(w[addr][0].key=="\0")
+		{
+			w[addr][0].key=s;
+			cout<<"Enter meaning of above keyword:-\t";
+			cin>>w[addr][0].value;
+		}
+		else if(w[addr][1].key=="\0")
+		{
+			w[addr][1].key=s;
+			cout<<"Enter meaning of above keyword:-\t";
+			cin>>w[addr][1].value;
+		}
+		else
+		{
+			int flg=1;
+			while(flg)
+			{
+				if (addr==25)
+					addr=0;
+				addr++;
+				if(w[addr][0].key=="\0")
+				{
+					w[addr][0].key=s;
+					cout<<"Enter meaning of above keyword:-\t";
+					cin>>w[addr][0].value;
+					flg=0;
+				}
+				else if(w[addr][1].key=="\0")
+				{
+					w[addr][1].key=s;
+					cout<<"Enter meaning of above keyword:-\t";
+					cin>>w[addr][1].value;
+					flg=0;
+				}
+			}
+		}
+	}
+}
+
+int dictionary::hashfunc(char c)
+{
+	int i;
+	i=c;
+	if(i>64 && i<96)
+		i=i+32;
+	i=i%97;
+	cout<<i<<endl;
+	return i;
+}
+
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	dictionary d1;
+	d1.input();
+	//d1.output();
 	return 0;
 }
